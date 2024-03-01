0af045d3d6f221c1ea60f76714f6d3b9915ade9c
Muhammad faraz <muhammad.faraz.ext@kavak.com>
2024-03-01 15:12:07 +0500

    Initial Commit added

---
+++ app.js
@@ -0,0 +1,24 @@
+// Import required modules
+const express = require('express');
+const bodyParser = require('body-parser');
+//const cors = require('cors');
+
+// Create an instance of Express
+const app = express();
+
+// Middleware to parse JSON requests
+app.use(bodyParser.json());
+
+// Middleware to enable CORS
+//app.use(cors());
+
+// Define a route
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
